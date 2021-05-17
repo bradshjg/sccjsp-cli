f1ec72505000f294f67689cacacad5242465f758
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2021-05-16 19:45:47 -0500

    Login automation

--- src/demo.py
+++ src/demo.py
@@ -1,23 +1,45 @@
+import csv
 from datetime import date, timedelta
+from getpass import getpass
 import os
 
+from bs4 import BeautifulSoup
 import requests
 
+LOGIN_AUTH_TOKEN_NAME = '__RequestVerificationToken'
+LOGIN_GET_URL = 'https://cjs.shelbycountytn.gov/CJS/Account/Login'
+LOGIN_POST_URL = 'https://cjs.shelbycountytn.gov/CJS/'
 
-def get_cookies():
-  COOKIE_ENV_VARS = {
-  "ASP.NET_SessionId": "SCCJSP_SESSION",
-  "FedAuth": "SCCJSP_FEDAUTH",
-  "FedAuth1": "SCCJSP_FEDAUTH1",
+
+class LoginFailed(Exception):
+  pass
+
+
+def login(session):
+  username = os.environ.get('SCCJSP_USERNAME') or input('username: ')
+  password = os.environ.get('SCCJSP_PASSWORD') or getpass('password: ')
+  login_get_resp = session.get(LOGIN_GET_URL)
+  login_get_parsed = BeautifulSoup(login_get_resp.content, 'html.parser')
+  token = login_get_parsed.find('input', {'name': LOGIN_AUTH_TOKEN_NAME})
+  login_data = {
+    LOGIN_AUTH_TOKEN_NAME: token.get('value'),
+    'UserName': username,
+    'Password': password
   }
-  return {
-    k: os.environ[v]
-    for k, v in COOKIE_ENV_VARS.items()
+  login_post_resp = session.post(login_get_resp.url, data=login_data)
+  login_post_parsed = BeautifulSoup(login_post_resp.content, 'html.parser')
+  sso_data = {
+    hidden_input.get('name'): hidden_input.get('value')
+    for hidden_input in login_post_parsed.find_all('input', {'type': 'hidden'})
   }
+  action = login_post_parsed.find('form').get('action')
+  if action != LOGIN_POST_URL:
+    raise LoginFailed
+  session.post(action, data=sso_data)
 
 def get_data():
   session = requests.session()
-  session.cookies.update(get_cookies())
+  login(session)
 
   tomorrow_formatted = (date.today() + timedelta(days=1)).strftime("%m/%d/%Y")
   session.post("https://cjs.shelbycountytn.gov/CJS/Hearing/SearchHearings/HearingSearch",
@@ -28,7 +50,7 @@ def get_data():
       "SearchCriteria.SelectedCourt": "All Locations",
       "SearchCriteria.SelectedHearingType": "All Hearing Types",
       "SearchCriteria.SearchByType": "Courtroom",
-      "SearchCriteria.SelectedCourtRoom": "1088",
+      "SearchCriteria.SelectedCourtRoom": "1088",  # Division 10
       "SearchCriteria.DateFrom": tomorrow_formatted,
       "SearchCriteria.DateTo": tomorrow_formatted,
     })
@@ -43,14 +65,17 @@ def get_data():
 
   return resp.json()
 
-def print_data(data):
-  for hearing in data["Data"]:
-    print(f'hearing for {hearing["DefendantName"]} at {hearing["HearingTime"]} on {hearing["HearingDate"]} with {hearing["JudgeParsed"]} presiding.')
+def write_data(data):
+  to_write = data['Data']
+  with open('data.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=to_write[0].keys())
+    writer.writeheader()
+    writer.writerows(to_write)
 
 
 def main():
   data = get_data()
-  print_data(data)
+  write_data(data)
 
 if __name__ == "__main__":
   main()
